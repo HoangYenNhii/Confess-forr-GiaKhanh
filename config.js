cb95b657de1d617528bffdc1589d2d2394c31cef
HoangYenNhii <106723880+HoangYenNhii@users.noreply.github.com>
2022-06-02 13:06:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
-const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+titleWeb: "for u 💕",
+    introTitle: 'gửi Gia Khánh💌',
+    introDesc: `Trái đất vốn lạ thường.
+    Mà sao em cứ đi nhầm đường.
+    Lạc vào tim anh lẻ loi.
+    Đằng sau chữ yêu đây là thương💓
+    Có một câu muốn hỏi từ rất lâu, cậu có thể trả lời kh? ^^`,
+    btnIntro: 'ok nè',
+    title: 'cảm ơn cậu vì đã mở chiếc cod này 💕',
+    desc: 'mỗi một người trong cuộc đời đều sẽ hội ngộ hai người, đến cuối cùng... một người ở trong tim và một người ở phương xa. Chỉ muốn hỏi cậu là đồng ý làm người yêu của tớ nhé🥺💕. Tớ đáng yêu như thế này, kh yêu thì phí lắm🥺 ',
+    btnYes: 'aiss, đồng ý nhé',
+    btnNo: 'mình làm bạn nha, bạn trai của cậu :>',
+    question: 'lí do cậu thích tớ',
+    btnReply: 'Gửi ',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+    mess: 'thông báo! tỏ tình thành công',
+    messDesc: 'nhấn enter để làm người yêu tớ',
+    btnAccept: 'enter',
+    messLink: 'https://www.facebook.com/messages/t/100030035348851'
